12359792c1a508b83c4931377fcc9edaeeba9d77
Jamkr <sauli.wani@gmail.com>
2019-03-11 08:32:40 +0800

    update helloworld

--- hellworld/helloworld.java
+++ hellworld/helloworld.java
@@ -1,6 +1,16 @@
 public class helloworld {
     public static void main(String[] args) {
+        int age = 29;
+        double weight = 67.8;
+        String name = "Jam";
+        String birthday = "1990-02-07";
+        boolean male = true;    //是否为男性
         System.out.println("hello world!");
-        System.out.println("x: " + 124);
+        System.out.println("url: http://afanihao.cn");
+        System.out.println("name: " + name);
+        System.out.println("birthday: " + birthday);
+        System.out.println("age: " + age);
+        System.out.println("weight: " + weight + "kg");
+        System.out.println("is a male: " + male);
     }
 }
